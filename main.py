49eef774aec47de7560e93653b656ac8149904f6
Jonah Breslow <jonah.breslow@mindbodyonline.com>
2023-01-24 20:41:48 -0800

    refactored

---
+++ main.py
@@ -0,0 +1,29 @@
+from nanograd.nn import MLP
+
+if __name__ == "__main__":
+    model = MLP(3, [4,4,1])
+
+    xs = [
+        [2.0, 3.0, -1.0],
+        [3.0, -1.0, 0.5],
+        [0.5, 1.0, 1.0],
+        [1.0, 1.0, -1.0]
+    ]
+
+    ys = [1.0, -1.0, -1.0, 1.0]
+
+    for k in range(120):
+        # forward pass
+        y_pred = [model(x) for x in xs]
+        loss = sum((y_out - y_gt)**2 for y_gt, y_out in zip(ys, y_pred))
+
+        # backward pass
+        model.zero_grad()
+        loss.backward()
+        
+        # update
+        for p in n.parameters():
+            p.data += -0.03 * p.grad
+        
+        if k % 10 == 0:
+            print(k, loss.data)
--- nanograd/value.py
+++
@@ -1,6 +0,0 @@
-class Value:
-    def __init__(self, data) -> None:
-        self.data = data
-
-    def __repr__(self) -> str:
-        return f"Value(data={self.data})"
